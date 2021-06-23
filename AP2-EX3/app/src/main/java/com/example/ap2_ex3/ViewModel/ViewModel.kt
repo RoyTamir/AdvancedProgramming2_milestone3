19efc72ed7d1d0c6bce2484ce86463563ec79615
רן תמ <4roytamir@gmail.com>
2021-06-23 14:42:53 +0300

    created basic vieowmodel and added stuff necessary for data binding

--- AP2-EX3/app/src/main/java/com/example/ap2_ex3/ViewModel/ViewModel.kt
+++ AP2-EX3/app/src/main/java/com/example/ap2_ex3/ViewModel/ViewModel.kt
@@ -1,15 +1,35 @@
 package com.example.ap2_ex3.ViewModel
 
-class ViewModel(name:String) {
-    val firstProperty = "First property: $name".also(::println)
+class ViewModel {
 
-    init {
-        println("First initializer block that prints ${name}")
+    private var port: Int = 0
+    private var ip: String? = null
+    private var rudder: Double = 0.0
+    private var throttle: Double = 0.0
+
+    // member function
+    fun setPort(p: Int) {
+        port = p
     }
-
-    val secondProperty = "Second property: ${name.length}".also(::println)
-
-    init {
-        println("Second initializer block that prints ${name.length}")
+    fun getPort(): Int {
+        return port
+    }
+    fun setIP(s: String) {
+        ip = s
+    }
+    fun getIP(): String {
+        return ip
+    }
+    fun setRudder(x: Double) {
+        rudder = x
+    }
+    fun getRudder(): Double {
+        return rudder
+    }
+    fun setThrottle(x: Double) {
+        throttle = x
+    }
+    fun getThrottle(): Double {
+        return throttle
     }
 }
